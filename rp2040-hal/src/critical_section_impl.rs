78395be4d50b1897b0a5d9ba35b16eda0688337e
Jan Niehusmann <jan@gondor.com>
2021-10-25 14:00:43 +0000

    Remove LOCK_COUNT
    LOCK_CORE with a sentinel value is sufficient to detect if this
    is the outermost lock.

--- rp2040-hal/src/critical_section_impl.rs
+++ rp2040-hal/src/critical_section_impl.rs
@@ -6,7 +6,6 @@ const LOCK_CORE_SENTINEL: u32 = 0xDEADBEEF;
 // If shared, we need to access them with interrupts disabled.
 // TODO: move away from static mut to avoid potential static init issues.
 static mut LOCK_CORE: u32 = LOCK_CORE_SENTINEL;
-static mut LOCK_COUNT: u8 = 0;
 
 unsafe impl critical_section::Impl for RpSpinlockCs {
     unsafe fn acquire() -> u8 {
@@ -15,10 +14,17 @@ unsafe impl critical_section::Impl for RpSpinlockCs {
         let core = (*pac::SIO::ptr()).cpuid.read().bits();
         // Need interrupts to be disabled to check/set our static variables
         cortex_m::interrupt::disable();
-        // Safety: LOCK_COUNT won't change if LOCK_CORE = current core
-        if LOCK_CORE == core && LOCK_COUNT != 0 {
+        let outermost_lock;
+        // Safety: if LOCK_CORE == current core, we know that the lock is
+        // acquired by us, and can't be released concurrently because interrupts are
+        // disabled.
+        // if LOCK_CORE != current one, the value of LOCK_CORE may change at any time,
+        // but it may not become equal to current core, again because interrupts on
+        // the current core are disabled.
+        if LOCK_CORE == core {
             // We already own the lock, so we must have called acquire within a critical_section.
             // Don't need to do anything here.
+            outermost_lock = false;
         } else {
             // Spin until we get the lock
             loop {
@@ -38,16 +44,18 @@ unsafe impl critical_section::Impl for RpSpinlockCs {
                 }
             }
             // If we broke out of the loop we have just acquired the lock.
+            outermost_lock = true;
         }
-        LOCK_COUNT += 1;
-        interrupts_active as _
+        if outermost_lock {
+            interrupts_active as _
+        } else {
+            2
+        }
     }
 
     unsafe fn release(token: u8) {
-        // Decrement our lock count
-        LOCK_COUNT -= 1;
         // Was this the outermost critical_section?
-        if LOCK_COUNT == 0 {
+        if token != 2 {
             // Yes, we were the last.
             // Set LOCK_CORE to the sentinel value to ensure the next call checks spinlock instead
             LOCK_CORE = LOCK_CORE_SENTINEL;
